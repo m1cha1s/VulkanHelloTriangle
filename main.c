eaf4ddd6696a8c0e9aa4927dddb76acbff18faf6
m1cha1s <msulej04@gmail.com>
2024-07-15 01:52:18 +0200

    Stopped at crating the surface

--- main.c
+++ main.c
@@ -5,15 +5,21 @@
 #include <assert.h>
 #include <vulkan/vulkan_core.h>
 
+//#define VK_USE_PLATFORM_X11_KHR
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_X11
+#include <GLFW/glfw3native.h>
 
 #define WIDTH 800
 #define HEIGHT 600
 
-static GLFWwindow* Window = NULL;
+static GLFWwindow* Win = NULL;
 static VkInstance Instance = { 0 };
 static VkPhysicalDevice PhyDev = VK_NULL_HANDLE;
+static VkDevice Dev;
+static VkQueue GraphicsQueue = VK_NULL_HANDLE;
+static VkSurfaceKHR Surface = VK_NULL_HANDLE;
 
 void initVulkan();
 void deinitVulkan();
@@ -23,7 +29,7 @@ int main() {
 
     initVulkan();
 
-    while (!glfwWindowShouldClose(Window)) {
+    while (!glfwWindowShouldClose(Win)) {
         glfwPollEvents();
     }
 
@@ -32,7 +38,29 @@ int main() {
     return 0;
 }
 
-static bool IsDeviceSuitable(VkPhysicalDevice Dev) {
+static uint32_t FindQueueFamilies() {
+    uint32_t Idx = 0;
+
+    uint32_t QueueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(PhyDev, &QueueFamilyCount, NULL);
+    VkQueueFamilyProperties QueueFamilies[QueueFamilyCount];
+    vkGetPhysicalDeviceQueueFamilyProperties(PhyDev, &QueueFamilyCount, QueueFamilies);
+
+    uint32_t CurrIdx = 1; // Starting to index from 1 so that 0 can act like "NULL".
+    for (uint32_t i = 0; i < QueueFamilyCount; i++) {
+        if (QueueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            Idx = CurrIdx;
+        }
+
+        if (Idx) break; // Short path to quit when found all queues.
+
+        CurrIdx++;
+    }
+
+    return Idx;
+}
+
+static bool IsDeviceSuitable() {
 #if 0 // NOTE(m1cha1s): We just want a Vulkan capable GPU.
     VkPhysicalDeviceProperties DevProps = { 0 };
     VkPhysicalDeviceFeatures DevFeatures = { 0 };
@@ -40,19 +68,16 @@ static bool IsDeviceSuitable(VkPhysicalDevice Dev) {
     vkGetPhysicalDeviceFeatures(PhyDev, &DevFeatures);
     return DevProps.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && DevFeatures.geometryShader;
 #else
-    return true;
+    uint32_t QueueIdx = FindQueueFamilies();
+    return QueueIdx; // Check if not 0.
 #endif
-}
-
-static uint32_t FindQueueFams() {
-
 }
 
 void initVulkan() {
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    Window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Hello Triangle", NULL, NULL);
+    Win = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Hello Triangle", NULL, NULL);
 
     // --- Vulkan shmoo start ---
 
@@ -116,13 +141,48 @@ void initVulkan() {
         exit(420);
     }
 
+    // --- Create logical device ---
 
+    uint32_t GraphicsIdx = FindQueueFamilies();
+
+    float QueuePriority = 1.0f;
+
+    VkDeviceQueueCreateInfo QueueCreateInfo = {
+        .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+        .queueFamilyIndex = GraphicsIdx,
+        .queueCount = 1,
+        .pQueuePriorities = &QueuePriority,
+    };
+
+    VkPhysicalDeviceFeatures DevFeatures = { 0 };
+    vkGetPhysicalDeviceFeatures(PhyDev, &DevFeatures);
+
+    VkDeviceCreateInfo DevCreateInfo = {
+        .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+        .pQueueCreateInfos = &QueueCreateInfo,
+        .queueCreateInfoCount = 1,
+        .pEnabledFeatures = &DevFeatures,
+        .enabledExtensionCount = 0,
+        .enabledLayerCount = 0,
+    };
+
+    if (vkCreateDevice(PhyDev, &DevCreateInfo, NULL, &Dev) != VK_SUCCESS) {
+        fprintf(stderr, "Failed to create a logical device.");
+        exit(6942);
+    }
+
+    vkGetDeviceQueue(Dev, GraphicsIdx, 0, &GraphicsQueue); // Retriving the queue to accualy do something with it.
+
+    VkDisplaySurfaceCreateInfoKHR SurfCreateInfo = {
+        .sType = VK_STRUCTURE_TYPE_DISPLAY_SURFACE_CREATE_INFO_KHR,
+    };
 }
 
 void deinitVulkan() {
+    vkDestroyDevice(Dev, NULL);
     vkDestroyInstance(Instance, NULL);
 
-    glfwDestroyWindow(Window);
+    glfwDestroyWindow(Win);
     glfwTerminate();
 
     printf("Bye!\n");
